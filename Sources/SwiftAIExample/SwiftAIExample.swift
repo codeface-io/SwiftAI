d2de2b422cc320b9608b275d59303db18c6ff5d9
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2024-11-02 21:08:14 +0100

    Simplify demo code

--- Sources/SwiftAIExample/SwiftAIExample.swift
+++ Sources/SwiftAIExample/SwiftAIExample.swift
@@ -1,38 +1,18 @@
 import SwiftAI
+import FoundationToolz
 
 @main
 struct ExampleApp {
     static func main() async {
-        let prompt = "What is the meaning of life? Keep your answer super concise and to the point :)"
-        
         do {
-            let answer = try await demonstrateAnthropic(prompt: prompt)
-            print(answer)
+            try await demonstrate()
         } catch {
             print(error)
         }
     }
-    
-    static func demonstrateAnthropic(prompt: String) async throws -> String  {
-        let response = try await Anthropic.Messages.post(
-            Anthropic.Messages.Request([.init(prompt)]),
-            authenticationKey: .anthropic
-        )
-        
-        return response.content.first?.text ?? ""
-    }
-    
-    static func demonstrateXAI(prompt: String) async throws -> String  {
-        let response = try await XAI.ChatCompletions.post(
-            XAI.ChatCompletions.Request([.init(prompt)]),
-            authenticationKey: .xAI
-        )
-        
-        return response.choices.first?.message?.content ?? ""
-    }
 }
 
-func demonstrate() async throws {
+func demonstrate() async throws(HTTP.RequestError) {
     let prompt = "What is the meaning of life? Be concise and to the point :)"
     
     let grokResponse = try await XAI.ChatCompletions.post(
