3405af1662adac71dcd93903acba5509d709072e
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2024-11-13 21:48:54 +0100

    Throw error when attempting to create API key with empty value

--- Sources/SwiftAI/API Key Storage/API.Key.swift
+++ Sources/SwiftAI/API Key Storage/API.Key.swift
@@ -1,14 +1,15 @@
 import Foundation
+import SwiftyToolz
 
 public enum API {
     /// a flexible and future proof way to configure and store api keys. a client does not need to make use of the flexibility and can for instance just assume one key per API without name and description
     public struct Key: Codable, Identifiable {
-        public init?(_ value: String,
+        public init(_ value: String,
              apiIdentifierValue: String,
              name: String? = nil,
              description: String? = nil,
-             id: UUID = UUID()) {
-            if value.isEmpty { return nil }
+             id: UUID = UUID()) throws {
+            if value.isEmpty { throw "Can not create an \(Self.self) with an empty value" }
             self.id = id
             self.value = value
             self.apiIdentifierValue = apiIdentifierValue
