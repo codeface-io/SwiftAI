3ecf04527477799e0b441e02063008df630dcc51
Sebastian Fichtner <sebastian@codeface.io>
2024-11-26 17:50:07 +0100

    Edit package file to make it easier to switch to local dependencies

--- Package.swift
+++ Package.swift
@@ -15,9 +15,11 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/flowtoolz/FoundationToolz.git", exact: "0.5.8"),
+        .package(url: "https://github.com/flowtoolz/FoundationToolz.git",
+                 exact: "0.5.8"),
+        .package(url: "https://github.com/flowtoolz/SwiftyToolz.git",
+                 exact: "0.5.6"),
 //        .package(path: "../FoundationToolz"),
-        .package(url: "https://github.com/flowtoolz/SwiftyToolz.git", exact: "0.5.6"),
 //        .package(path: "../SwiftyToolz"),
     ],
     targets: [
